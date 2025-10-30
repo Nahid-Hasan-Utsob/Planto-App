498bc68a68bf46409e3881afcaa08dba83b96ba4
Nahid Hasan Utsob <mdnahidhassan555@gmail.com>
2025-10-30 13:59:43 +0600

    fix: add margin to cart NavLink for improved spacing in Navbar_Top component

--- src/components/Navbar_Top.tsx
+++ src/components/Navbar_Top.tsx
@@ -55,7 +55,7 @@ export default function Navbar_Top() {
               <IoIosSearch />
             </div>
 
-            <NavLink to="cart" className="relative">
+            <NavLink to="cart" className="relative lg:mr-4">
               <SlHandbag className="text-2xl" />
               {totalCount > 0 && (
                 <span className="absolute -top-2 -right-2 bg-red-500 text-white text-xs rounded-full px-2">
