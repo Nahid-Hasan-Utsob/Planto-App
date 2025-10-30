9f7918096e1b28950ca236ba3c0412c426bef6d7
Nahid Hasan Utsob <mdnahidhassan555@gmail.com>
2025-10-30 13:47:26 +0600

    fix: update padding and font size in SearchButton component for improved responsiveness

--- src/components/SearchButton.tsx
+++ src/components/SearchButton.tsx
@@ -9,7 +9,7 @@ interface SearchButtonProps {
 const SearchButton: React.FC<SearchButtonProps> = ({
   onClick,
   label = "Search",
-  className = "mt-3 w-full bg-yellow-400 hover:bg-yellow-500 text-black font-semibold py-2 rounded-md transition"
+  className = "mt-3 w-full bg-yellow-400 hover:bg-yellow-500 text-black font-semibold md:py-2 py-1 text-[13px] md:text-base rounded-md transition"
 }) => {
   return (
     <button onClick={onClick} className={className}>
